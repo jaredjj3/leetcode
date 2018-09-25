c807b1488f05aa79ead199272468c98b53b8e8ec
Jared Johnson <jared@nuts.com>
2018-09-25 07:29:24 -0400

    Make skeleton more generic.

--- _skeleton.rb
+++ _skeleton.rb
@@ -1,21 +1,10 @@
 require "minitest/autorun"
 
-# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-# You may assume that each input would have exactly one solution, and you may not use the same element twice.
-# Example:
-# Given nums = [2, 7, 11, 15], target = 9,
-# Because nums[0] + nums[1] = 2 + 7 = 9,
-# return [0, 1].
-
-# @param {Integer[]} nums
-# @param {Integer} target
-# @return {Integer[]}
-def two_sum(nums, target)
-
+def foo
 end
 
-describe "#two_sum" do
-  it "works" do
-    assert_nil(two_sum("foo", "bar"))
+describe "#foo" do
+  it "always returns nil" do
+    assert_nil(foo)
   end
 end
