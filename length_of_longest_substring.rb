c15e3194ffb76d1b512450fe306de3d3e5489ccb
Jared Johnson <jared@nuts.com>
2018-09-25 20:05:01 -0400

    Finish length_of_longest_substring.rb.

--- length_of_longest_substring.rb
+++ length_of_longest_substring.rb
@@ -18,8 +18,23 @@ require "minitest/autorun"
 # Explanation: The answer is "wke", with the length of 3. 
 #              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
+require "set"
+
+# SOLUTION 1: O(n**2) time, O(n) space
+#
+# @param {String} s
+# @return {Integer}
 def length_of_longest_substring(str)
-    
+  chars = str.chars
+  max = 0
+
+  chars.each.with_index do |start_char, start_ndx|
+    seen = Set.new([start_char])
+    chars[(start_ndx + 1)..-1].each.with_index { |end_char, end_ndx| break unless seen.add?(end_char) }
+    max = [seen.size, max].max
+  end
+
+  max
 end
 
 describe "#length_longest_substring" do
@@ -27,5 +42,6 @@ describe "#length_longest_substring" do
     assert_equal(3, length_of_longest_substring("abcabcbb"))
     assert_equal(1, length_of_longest_substring("bbbbb"))
     assert_equal(3, length_of_longest_substring("pwwkew"))
+    assert_equal(8, length_of_longest_substring("ddjfkkqwertyaa"))
   end
 end
