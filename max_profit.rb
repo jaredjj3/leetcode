af748ba7bd2718ead2b2d9d42503bb1c888c3adc
jaredjj3 <jared@nuts.com>
2018-11-05 10:28:09 -0500

    Complete max_profit.

--- max_profit.rb
+++ max_profit.rb
@@ -22,15 +22,43 @@ Output: 0
 Explanation: In this case, no transaction is done, i.e. max profit = 0.
 =end
 
+# Let p be the number of prices in +prices+
+# O(n**2) time, O(1) space
+# def max_profit(prices)
+#   max = 0
+
+#   prices.each.with_index do |buy_price, ndx|
+#     prices[(ndx + 1)..-1].each do |sell_price|
+#       profit = sell_price - buy_price
+#       max = [max, profit].max
+#     end
+#   end
+
+#   max
+# end
+
+# Let p be the number of prices in +prices+
+# O(n) time, O(1) space
 def max_profit(prices)
+  max = 0
+  min = Float::INFINITY
+
+  prices.each do |price|
+    min = [min, price].min
+    max = [max, price - min].max
+  end
+
+  max
 end
 
 describe "#max_profit" do
   it "solves the problem prompt" do
     assert_equal(0, max_profit([7, 1]))
     assert_equal(6, max_profit([1, 7]))
-    assert_equal(0, max_profit([1, 2, 3]))
-    assert_equal(2, max_profit([3, 2, 1]))
+    assert_equal(2, max_profit([1, 2, 3]))
+    assert_equal(0, max_profit([3, 2, 1]))
     assert_equal(10, max_profit([6, 2, 3, 4, 2, 1, 11]))
+    assert_equal(5, max_profit([7, 1, 5, 3, 6, 4]))
+    assert_equal(2, max_profit([2, 1, 2, 1, 0, 1, 2]))
   end
 end
