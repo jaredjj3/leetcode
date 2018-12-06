ee714ecb5191f617e34c2fcb426903ca255b43fb
Jared Johnson <jared@nuts.com>
2018-12-06 14:14:09 -0500

    Finish check_contiguous.

--- lib/check_contiguous.rb
+++ lib/check_contiguous.rb
@@ -1,5 +1,6 @@
 require "minitest/autorun"
 require "byebug"
+require "set"
 
 # PROMPT
 =begin
@@ -7,23 +8,43 @@ This method returns true if all similar elements of an array are contiguous and 
 if they are not.
 =end
 
-def contiguous?
+def contiguous?(arr)
+  probe = arr.first
+  set = Set.new
+
+  arr.all? do |el|
+    next false if set.include?(el) && el != probe
+    set.add(el)
+    probe = el
+  end
+end
+
+def check_contiguous
+  probe = arr.first
+  set = Set.new
+  errors = []
+
+  arr.each do |el|
+    errors << "Error" if set.include?(el) && el != probe
+    set.add(el)
+    probe = el
+  end
 end
 
 describe "#contiguous?" do
   it "returns true for contiguous arrays" do
-    assert(contiguous?([1, 1, 1, 2, 2, 2, 3, 4, 4]))
-    assert(contiguous?([2, 2, 3, 3, 7, 7, 6, 6]))
-    assert(contiguous?([1, 2, 3]))
+    assert_equal(true, contiguous?([1, 1, 1, 2, 2, 2, 3, 4, 4]))
+    assert_equal(true, contiguous?([2, 2, 3, 3, 7, 7, 6, 6]))
+    assert_equal(true, contiguous?([1, 2, 3]))
   end
 
   it "returns false for non contiguous arrays" do
-    refute(contiguous?([1, 1, 1, 2, 2, 2, 1, 3, 4, 4]))
-    refute(contiguous?([2, 2, 3, 3, 7, 7, 2, 2, 6, 6]))
-    refute(contiguous?([1, 2, 3, 1]))
+    assert_equal(false, contiguous?([1, 1, 1, 2, 2, 2, 1, 3, 4, 4]))
+    assert_equal(false, contiguous?([2, 2, 3, 3, 7, 7, 2, 2, 6, 6]))
+    assert_equal(false, contiguous?([1, 2, 3, 1]))
   end
 
   it "returns true for empty arrays" do
-    assert(contiguous?([]))
+    assert_equal(true, contiguous?([]))
   end
 end
