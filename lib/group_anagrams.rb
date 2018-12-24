13d3379b11f569b901b04dd74d6135ad572b57c9
Jared Johnson <jared@nuts.com>
2018-12-23 20:46:24 -0500

    Add group_anagrams.

---
+++ lib/group_anagrams.rb
@@ -0,0 +1,30 @@
+require "minitest/autorun"
+require "byebug"
+
+# PROMPT
+=begin
+Given an array of strings, group anagrams together.
+
+Example:
+
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note:
+
+All inputs will be in lowercase.
+The order of your output does not matter.
+=end
+
+def group_anagrams
+end
+
+describe "#group_anagrams" do
+  it "solves the problem prompt" do
+    flunk
+  end
+end
