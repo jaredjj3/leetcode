abe29158908d0f577c6a8a87516ba039bf4cc26f
Jared Johnson <jared@nuts.com>
2018-12-26 17:52:17 -0500

    Finish maximum_swap.

--- lib/maximum_swap.rb
+++ lib/maximum_swap.rb
@@ -17,11 +17,30 @@ Note:
 The given number is in the range [0, 108]
 =end
 
-def maximum_swap
+def maximum_swap(num)
+  max = num
+  chars = num.to_s.split("")
+
+  chars.each_index do |ndx1|
+    chars.each_index do |ndx2|
+      dup_chars = chars.dup
+      tmp = dup_chars[ndx1]
+      dup_chars[ndx1] = dup_chars[ndx2]
+      dup_chars[ndx2] = tmp
+      max = [max, dup_chars.join.to_i].max
+    end
+  end
+
+  max
 end
 
 describe "#maximum_swap" do
   it "solves the problem prompt" do
-    flunk
+    assert_equal(7236, maximum_swap(2736))
+    assert_equal(9973, maximum_swap(9973))
+  end
+
+  it "solves failing leetcode tests" do
+    assert_equal(98863, maximum_swap(98368))
   end
 end
