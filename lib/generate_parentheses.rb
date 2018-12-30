7a9a1fa97267ef43f8e04f38b9dfb83be1985009
Jared Johnson <jared@nuts.com>
2018-12-29 20:56:40 -0500

    Create generate_parentheses.

---
+++ lib/generate_parentheses.rb
@@ -0,0 +1,26 @@
+require "minitest/autorun"
+require "byebug"
+
+# PROMPT
+=begin
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+=end
+
+def generate_parentheses
+end
+
+describe "#generate_parentheses" do
+  it "solves the problem prompt" do
+    flunk
+  end
+end
