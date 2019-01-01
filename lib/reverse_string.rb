a1b54a5eb941f295d36a2e781655958082184586
Jared Johnson <jared@nuts.com>
2019-01-01 16:58:04 -0500

    Finish reverse string.

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -16,6 +16,18 @@ Output: "amanaP :lanac a ,nalp a ,nam A"
 =end
 
 def reverse_string(str)
+  chars = str.chars
+  mid = str.size / 2
+  ndx = 0
+  while ndx < mid
+    r_ndx = -(ndx + 1)
+    tmp = chars[ndx]
+    chars[ndx] = chars[r_ndx]
+    chars[r_ndx] = tmp
+    ndx += 1
+  end
+
+  chars.join
 end
 
 describe "#reverse_string" do
