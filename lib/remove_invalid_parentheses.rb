61bb305aa6c0bcbeb42e3fc524447e2956ad5e7f
Jared Johnson <jared@nuts.com>
2019-01-01 17:00:26 -0500

    Create remove_invalid_parens.

---
+++ lib/remove_invalid_parentheses.rb
@@ -0,0 +1,31 @@
+require "minitest/autorun"
+require "byebug"
+
+# PROMPT
+=begin
+Remove the minimum number of invalid parentheses in order to make the input string valid. Return all possible results.
+
+Note: The input string may contain letters other than the parentheses ( and ).
+
+Example 1:
+
+Input: "()())()"
+Output: ["()()()", "(())()"]
+Example 2:
+
+Input: "(a)())()"
+Output: ["(a)()()", "(a())()"]
+Example 3:
+
+Input: ")("
+Output: [""]
+=end
+
+def remove_invalid_parentheses
+end
+
+describe "#remove_invalid_parentheses" do
+  it "solves the problem prompt" do
+    flunk
+  end
+end
