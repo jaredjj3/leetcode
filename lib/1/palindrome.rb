c9d845732977748fc4ce6188fc9586042a3ed979
Jared Johnson <jared@nuts.com>
2019-01-15 19:41:02 -0500

    Finish palindrome.

---
+++ lib/1/palindrome.rb
@@ -0,0 +1,14 @@
+require "minitest/autorun"
+
+# SESSION
+# 1
+
+# PROMPT
+=begin
+Create a method that determines if a string is a palindrome or not.
+=end
+
+def palindrome(str)
+  sanitized = str.scan(/[a-zA-Z]/).map!(&:downcase)
+  sanitized == sanitized.reverse
+end
---
+++ test/1/palindrome_test.rb
@@ -0,0 +1,26 @@
+require_relative "../../lib/1/palindrome.rb"
+
+# SESSION
+# 1
+
+describe "#palindrome" do
+  it "returns true for strings that are palindromes" do
+    assert(palindrome("tacocat"))
+    assert(palindrome("racecar"))
+  end
+
+  it "ignores punctuation" do
+    assert(palindrome("t#a co! cat!"))
+    assert(palindrome("race,car?"))
+  end
+
+  it "ignores captilization" do
+    assert(palindrome("TaCocat"))
+    assert(palindrome("RACEcar"))
+  end
+
+  it "returns false for strings that are not palindromes" do
+    refute(palindrome("asdf"))
+    refute(palindrome("hello world"))
+  end
+end
