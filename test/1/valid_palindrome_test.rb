4cf42b98c2600421f5b1ed7624e18afb6fd3a929
Jared Johnson <jared@nuts.com>
2019-01-19 14:50:36 -0500

    Test valid_palindrome.

--- test/1/valid_palindrome_test.rb
+++ test/1/valid_palindrome_test.rb
@@ -5,7 +5,18 @@ require_relative "../../lib/1/valid_palindrome.rb"
 # 1
 
 describe "#valid_palindrome" do
-  it "solves the problem prompt" do
-    flunk
+  it "returns true if the original string is a palindrome" do
+    assert(valid_palindrome("aba"))
+    assert(valid_palindrome("abba"))
+  end
+
+  it "returns true if a single character can be removed to make a palindrome" do
+    assert(valid_palindrome("abca"))
+    assert(valid_palindrome("abbca"))
+  end
+
+  it "returns false if it takes more than one character to make a palindrome" do
+    refute(valid_palindrome("abcca"))
+    refute(valid_palindrome("abcbca"))
   end
 end
