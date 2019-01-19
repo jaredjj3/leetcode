3e8ad2efb343f742d55370d6e823a8c1e94727ad
Jared Johnson <jared@nuts.com>
2019-01-19 14:32:27 -0500

    Support numeric characters in #palindrome.

--- lib/1/palindrome.rb
+++ lib/1/palindrome.rb
@@ -9,6 +9,6 @@ Create a method that determines if a string is a palindrome or not.
 =end
 
 def palindrome(str)
-  sanitized = str.scan(/[a-zA-Z]/).map!(&:downcase)
+  sanitized = str.scan(/[a-zA-Z0-9]/).map!(&:downcase)
   sanitized == sanitized.reverse
 end
