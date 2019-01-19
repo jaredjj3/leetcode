883584388bf6144e7d8748e0328aba9008b43ab8
Jared Johnson <jared@nuts.com>
2019-01-19 14:45:47 -0500

    Create valid_palindrome.

---
+++ lib/1/valid_palindrome.rb
@@ -0,0 +1,12 @@
+require_relative "../data_structures/data_structures.rb"
+
+# SESSION
+# 1
+
+# PROMPT
+=begin
+
+=end
+
+def valid_palindrome
+end
---
+++ test/1/valid_palindrome_test.rb
@@ -0,0 +1,11 @@
+require "minitest/autorun"
+require_relative "../../lib/1/valid_palindrome.rb"
+
+# SESSION
+# 1
+
+describe "#valid_palindrome" do
+  it "solves the problem prompt" do
+    flunk
+  end
+end
