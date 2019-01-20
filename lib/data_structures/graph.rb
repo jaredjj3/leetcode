c092d73e2e73dd6dfea37481dc25a34647867e07
Jared Johnson <jared@nuts.com>
2019-01-20 13:39:47 -0500

    Simplify graph api.

--- lib/data_structures/graph.rb
+++ lib/data_structures/graph.rb
@@ -18,92 +18,36 @@ adjacency list or matrix.
 
 class Graph
   class Node
-    attr_reader :key
+    attr_reader :key, :children
     attr_accessor :value
 
     def initialize(key, value)
       @key = key
       @value = value
+      @children = Set.new
     end
   end
 
-  attr_reader :matrix, :list
+  attr_reader :nodes
 
   def initialize
     @nodes = {}
-    @index_by_key = {}
-    @matrix = []
-    @list = {}
   end
 
-  def get(key)
+  def [](key)
     @nodes[key]
   end
 
-  def nodes
-    @nodes.values
+  def include?(key)
+    @nodes.key?(key)
   end
 
-  def has?(key)
-    @list.key?(key)
+  def []=(key, value)
+    @nodes[key] ||= Node.new(key, value)
+    @nodes[key].value = value
   end
 
-  def add(key, value)
-    if has?(key)
-      get(key).value = value
-    else
-      node = Node.new(key, value)
-      @index_by_key[key] = @nodes.size
-      @nodes[key] = node
-      @list[key] = []
-      @matrix << []
-      @matrix.each { |row| row.fill(false, row.size...@matrix.size) }
-    end
-
-    value
+  def delete(key)
+    @nodes.delete(key)&.value
   end
-
-  def remove(key)
-    return nil unless has?(key)
-
-    value = get(key).value
-    ndx = index(key)
-    @matrix.delete_at(ndx)
-    @matrix.each { |row| row.delete_at(ndx) }
-    @list.delete(key)
-    @nodes.reject! { |node_key| node_key == key }
-
-    value
-  end
-
-  def connect(key1, key2)
-    return false if !has?(key1) || !has?(key2)
-    return false if connected?(key1, key2)
-
-    @list[key1] << get(key2)
-
-    ndx1 = index(key1)
-    ndx2 = index(key2)
-    @matrix[ndx1][ndx2] = true
-
-    true
-  end
-
-  def connected?(key1, key2)
-    ndx1 = index(key1)
-    ndx2 = index(key2)
-    
-    return false if ndx1.nil? || ndx2.nil?
-    @matrix[ndx1][ndx2]
-  end
-
-  def children(key)
-    @list[key]
-  end
-
-  private
-
-    def index(key)
-      @index_by_key[key]
-    end
 end
--- test/data_structures/graph_test.rb
+++ test/data_structures/graph_test.rb
@@ -6,79 +6,57 @@ require_relative "../../lib/data_structures/graph.rb"
 
 describe "Graph" do
   describe "::initialize" do
-    it "inits #matrix and #list" do
+    it "inits #nodes" do
       graph = Graph.new
       
-      assert_equal([], graph.matrix)
-      assert_equal({}, graph.list)
+      assert_equal({}, graph.nodes)
     end
   end
 
-  describe "#get" do
+  describe "#[]" do
     it "returns nil if the key is not present in the graph" do
       graph = Graph.new
 
-      assert_nil(graph.get(1))
+      assert_nil(graph[1])
     end
 
     it "returns the value of a node with the given key" do
       graph = Graph.new
       value = Object.new
-      graph.add(1, value)
+      graph[1] = value
 
-      node = graph.get(1)
+      node = graph[1]
       assert_equal(1, node.key)
       assert_equal(value, node.value)
     end
   end
 
-  describe "#has?" do
+  describe "#include?" do
     it "returns a boolean corresponding to the presence of a key in the graph" do
       graph = Graph.new
-      graph.add(1, 1)
+      graph[1] = 1
 
-      assert(graph.has?(1))
-      refute(graph.has?(2))
+      assert(graph.include?(1))
+      refute(graph.include?(2))
     end
   end
 
-  describe "#add" do
+  describe "#[]=" do
     it "returns the value that was added" do
       graph = Graph.new
       value = Object.new
 
-      assert_equal(value, graph.add(1, value))
+      assert_equal(value, graph[1] = value)
     end
 
     it "adds a node to #nodes" do
       graph = Graph.new
       value = Object.new
-      graph.add(1, value)
+      graph[1] = value
       nodes = graph.nodes
 
       assert_equal(1, nodes.size)
-      assert_equal(value, nodes.first.value)
-    end
-
-    it "adds the node to #matrix" do
-      graph = Graph.new
-      graph.add(1, 1)
-      matrix = graph.matrix
-
-      assert_equal(1, matrix.size)
-      assert_equal(1, matrix.first.size)
-      refute(matrix.first.first)
-    end
-
-    it "adds the node to #list" do
-      graph = Graph.new
-      value = Object.new
-      graph.add(1, value)
-      list = graph.list
-
-      assert_equal(1, list.size)
-      assert_equal([], list.fetch(1))
-      assert_equal([1], list.keys)
+      assert_equal(value, nodes[1].value)
     end
 
     it "replaces the value of an existing key" do
@@ -87,122 +65,31 @@ describe "Graph" do
       value2 = Object.new
 
       refute_equal(value1, value2)
-      graph.add(1, value1)
-      assert_equal(value1, graph.get(1).value)
-      graph.add(1, value2)
-      assert_equal(value2, graph.get(1).value)
+      graph[1] = value1
+      assert_equal(value1, graph[1].value)
+      graph[1] = value2
+      assert_equal(value2, graph[1].value)
     end
   end
 
-  describe "#remove" do
-    it "returns the value that was removed" do
+  describe "#delete" do
+    it "returns the value that was deleted" do
       graph = Graph.new
       value = Object.new
-      graph.add(1, value)
+      graph[1] = value
 
-      assert_equal(value, graph.remove(1))
-      assert_nil(graph.remove(1))
+      assert_equal(value, graph.delete(1))
+      assert_nil(graph.delete(1))
     end
 
-    it "removes the node from #nodes" do
+    it "deletes the node from #nodes" do
       graph = Graph.new
       value = Object.new
-      graph.add(1, value)
+      graph[1] = value
 
       assert_equal(1, graph.nodes.size)
-      graph.remove(1)
+      graph.delete(1)
       assert_equal(0, graph.nodes.size)
-    end
-
-    it "removes the node from #list" do
-      graph = Graph.new
-      value = Object.new
-      graph.add(1, value)
-
-      assert_equal(1, graph.list.size)
-      graph.remove(1)
-      assert_equal(0, graph.nodes.size)
-    end
-
-    it "removes the node from #matrix" do
-      graph = Graph.new
-      value = Object.new
-      graph.add(1, value)
-
-      assert_equal(1, graph.matrix.size)
-      assert_equal(1, graph.matrix.first.size)
-      graph.remove(1)
-      assert_equal(0, graph.matrix.size)
-    end
-  end
-
-  describe "#connect" do
-    it "returns a boolean correspond to if a new connection was made from key1 to key2" do
-      graph = Graph.new
-      graph.add(1, Object.new)
-      graph.add(2, Object.new)
-
-      assert(graph.connect(1, 2))
-      refute(graph.connect(1, 2))
-      
-      assert(graph.connect(2, 1))
-      refute(graph.connect(2, 1))
-
-      assert(graph.connect(1, 1))
-      refute(graph.connect(1, 1))
-    end
-
-    it "does not change #nodes" do
-      graph = Graph.new
-      graph.add(1, Object.new)
-      graph.add(2, Object.new)
-
-      before = [*graph.nodes]
-      graph.connect(1, 2)
-      after = [*graph.nodes]
-      assert_equal(before, after)
-    end
-
-    it "updates #list to include the new edge" do
-      graph = Graph.new
-      value1 = Object.new
-      value2 = Object.new
-      graph.add(1, value1)
-      graph.add(2, value2)
-      
-      assert_equal([], graph.list.fetch(1))
-      assert_equal([], graph.list.fetch(2))
-      graph.connect(1, 2)
-      assert_equal([value2], graph.list.fetch(1).map(&:value))
-      assert_equal([], graph.list.fetch(2))
-    end
-
-    it "updates #matrix to include the new edge" do
-      graph = Graph.new
-      graph = Graph.new
-      graph.add(1, 1)
-      graph.add(2, 2)
-
-      assert_equal([[false, false], [false, false]], graph.matrix)
-      graph.connect(1, 2)
-      assert_equal([[false, true], [false, false]], graph.matrix)
-    end
-  end
-
-  describe "#children" do
-    it "returns the children for a given key" do
-      graph = Graph.new
-      graph = Graph.new
-      value1 = Object.new
-      value2 = Object.new
-      graph.add(1, value1)
-      graph.add(2, value2)
-
-      assert_equal([], graph.children(1))
-      assert_equal([], graph.children(2))
-      graph.connect(1, 2)
-      assert_equal([value2], graph.children(1).map(&:value))
-      assert_equal([], graph.children(2))
     end
   end
 end
